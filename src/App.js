7ef14732c0f0c0f1e143fea1c2c0a35b2090ec1b
Swapneel Sahu <Swapneel.Sahu@in.ey.com>
2024-02-08 15:32:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [message, setMessage] = useState('');
+
+	const fetchMessage = async () => {
+	const response = await axios.get('http://localhost:5000/api/message');
+	setMessage(response.data.message)
+    };
+
+    return (
+        <div ClassName = "App">
+            <header className = "AppHeader">
+                <p>{message}</p>
+                <button onClick={fetchMessage}>Get Message</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
